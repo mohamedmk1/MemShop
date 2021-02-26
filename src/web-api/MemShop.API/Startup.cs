f570ba3a47031132c978fc7ea484b6512692c1dc
Mohamed EL MABROUK <m.el-mabrouk@groupeonepoint.com>
2021-02-26 18:32:29 +0100

    Add swagger

--- src/web-api/MemShop.API/Startup.cs
+++ src/web-api/MemShop.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 
 namespace MemShop.API
@@ -37,6 +38,13 @@ namespace MemShop.API
             services.AddScoped<ICategoryInfoRepository, CategoryInfoRepository>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //Swagger
+            services.AddOptions();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MemShop API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +66,12 @@ namespace MemShop.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MemShop API V1");
             });
         }
     }
