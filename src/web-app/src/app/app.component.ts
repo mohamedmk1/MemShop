04793ca561e60b70eb4c274ebf826071851e4a58
Mohamed EL MABROUK <m.el-mabrouk@groupeonepoint.com>
2021-03-02 13:20:55 +0100

    web-app: log init application action when application started

--- src/web-app/src/app/app.component.ts
+++ src/web-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppState} from './app-state';
+import {Store} from '@ngrx/store';
+import {applicationInit} from './core/store/actions/core.actions';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web-app';
+export class AppComponent implements OnInit {
+    title = 'web-app';
+
+    constructor(private readonly store: Store<AppState>) {}
+
+    ngOnInit(): void {
+        this.store.dispatch(applicationInit());
+    }
 }
