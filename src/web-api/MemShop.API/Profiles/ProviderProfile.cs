71ebd68c98af2eadca4d39e9f219649dcb9715dd
Amir Juba ZAIR <aj.zair@groupeonepoint.com>
2021-03-05 10:57:42 +0100

    Add Provider Dto

---
+++ src/web-api/MemShop.API/Profiles/ProviderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemShop.API.Profiles
+{
+    public class ProviderProfile : Profile
+    {
+        public ProviderProfile()
+        {
+            CreateMap<Entities.Provider, Models.ProviderDto>()
+                .ReverseMap();
+        }
+        
+    }
+}
