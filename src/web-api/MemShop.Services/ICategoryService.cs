e894e7be36c38219bffbcb9232d3050a6cf2d123
Amir Juba ZAIR <aj.zair@groupeonepoint.com>
2021-03-11 14:33:03 +0100

    Add Service Porject

---
+++ src/web-api/MemShop.Services/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using MemShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemShop.Services
+{
+    public interface ICategoryService
+    {
+        Category GetCategoryById(int id);
+        Category CreateCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(Category category);
+        Category GetByIdWithProducts(int id);
+        void AddProductForCategory(int categoryId, Product product);
+        bool IsCategoryExist(int categoryId);
+    }
+}
---
+++ src/web-api/MemShop.Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using MemShop.Data;
+using MemShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemShop.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+        public Product CreateProduct(Product product)
+        {
+            _unitOfWork.Products.Add(product);
+            _unitOfWork.Commit();
+            return product;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _unitOfWork.Products.Remove(product);
+        }
+
+        public IEnumerable<Product> GetAllByCategoryId(int categoryId)
+        {
+            return _unitOfWork.Products.GetAllByCategoryId(categoryId);
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _unitOfWork.Products.GetById(id);
+        }
+
+        public Product GetProductForCategory(int categoryId, int productId)
+        {
+            return _unitOfWork.Products
+                .GetProductForCategory(categoryId, productId);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _unitOfWork.Products.Update(product);
+        }
+    }
+}
